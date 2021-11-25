0fcfea9fd33ee00aff283fbcff6b61fe005e29ed
elnoor <elnoormobile@gmail.com>
2021-11-24 23:03:08 -0700

    bootstrap imported

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./../node_modules/bootstrap/scss/bootstrap";
+import "./assets/scss/customizedBootstrap.scss";
+import "./assets/scss/style.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
