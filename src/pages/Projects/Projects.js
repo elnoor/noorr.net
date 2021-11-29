914823fa075a8c99bf8aba322af08a2db13c9740
elnoor <elnoormobile@gmail.com>
2021-11-28 20:06:41 -0700

    projects page done; run game added into public

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,32 +1,62 @@
 import React from "react";
 import { projectsData } from "./projectsData";
 
-
 export default function Projects() {
   return (
     <div className="row">
+      <div className="col-12 text-center">
+        <p>
+          Some of the projects that i built just to have fun when I had some
+          spare time.
+        </p>
+      </div>
       {projectsData.map((project, index) => {
         return (
-          <div key={index} className="col-12">
-            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-              <div className="col p-3 d-flex flex-column position-static">
-                <h5 className="mb-1">{project.name}</h5>
-                <p className="card-text mb-auto">{project.description}</p>
-                {project.link && (
-                  <a
-                    href={project.link}
-                    className="btn btn-outline-info d-block width-fit-content mt-3"
-                  >
-                    View
-                  </a>
-                )}
-              </div>
-              <div className="col-auto d-none d-lg-block">
-                {project.image ? (
-                  <img src={project.image} alt={project.name} />
-                ) : (
-                  getPlaceholderSvg()
-                )}
+          <div key={index} className="col-md-6 mt-4">
+            <div className="card shadow bg-dark border border-custom-color overflow-hidden">
+              {project.image ? (
+                <img
+                  src={project.image}
+                  alt={project.name}
+                  style={{
+                    height: "17rem",
+                    objectFit: "cover",
+                    objectPosition: "top",
+                  }}
+                />
+              ) : (
+                getPlaceholderSvg()
+              )}
+              <div className="card-body">
+                <h5 className="card-title text-light text-opacity-75">
+                  {project.name}
+                </h5>
+                <p className="card-text">{project.description}</p>
+                <div className="d-flex">
+                  {project.link && (
+                    <a
+                      href={project.link}
+                      className="btn btn-outline-info btn-sm"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {project.linkText ? project.linkText : "View"}
+                    </a>
+                  )}
+                  {!project.active && (
+                    <small className="text-warning text-opacity-25">
+                      Removed, this project is not active
+                    </small>
+                  )}
+                  {project.year && (
+                    <small
+                      className="text-muted ms-auto"
+                      title="The year it was developed"
+                    >
+                      {project.year}
+                    </small>
+                  )}
+                </div>
               </div>
             </div>
           </div>
@@ -39,19 +69,19 @@ export default function Projects() {
 const getPlaceholderSvg = () => {
   return (
     <svg
-      className="bd-placeholder-img"
-      width="200"
-      height="250"
+      className="bd-placeholder-img card-img-top"
+      width="100%"
+      height="17rem"
       xmlns="http://www.w3.org/2000/svg"
       role="img"
-      aria-label="Placeholder: Thumbnail"
+      aria-label="Placeholder: No Image"
       preserveAspectRatio="xMidYMid slice"
       focusable="false"
     >
       <title>Placeholder</title>
       <rect width="100%" height="100%" fill="#55595c"></rect>
-      <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-        Thumbnail
+      <text x="50%" y="50%" fill="#eceeef" dy=".3em" dx="-1.8rem">
+        No Image
       </text>
     </svg>
   );
--- src/pages/Projects/projectsData.js
+++ src/pages/Projects/projectsData.js
@@ -1,13 +1,83 @@
 import capsAzImg from "./images/capsaz.png";
+import menoteImg from "./images/menote.png";
+import jewelImg from "./images/jewel.png";
+import runImg from "./images/run.png";
+import elandiyaImg from "./images/elandiya.png";
+import milkywayImg from "./images/milkyway.png";
+import angoraImg from "./images/angora.png";
+import snakeImg from "./images/snake.png";
 
 export const projectsData = [
   {
     active: true,
+    name: "Candy Snake",
+    image: snakeImg,
+    year: 2020,
+    link: "https://candysnake.netlify.app",
+    linkText: "Play",
+    description:
+      "This different snake game is built using JavaScript and React (hooks) as well as HTML and SCSS. Some AWS Lambda and FaunaDB has been used to store Top Scorers. I have made this project open source at GitHub.",
+  },
+  {
+    active: false,
     name: "Caps.Az",
     image: capsAzImg,
-    link: "https://caps-az.com",
+    year: 2017,
     description:
       "Caps.Az is a funny content sharing platform where images, gif animations, YouTube videos and texts are being shared daily by users. Although anyone can view and enjoy these posts only registered users are allowed to like, comment them. Built-in powerful admin panel allows admins to customize application as well as managing content. Caps.Az has been developed using PHP 7, MySQL, JavaScript, jQuery, HTML, CSS and Bootstrap. Webview based Android mobile app has been also built using Java and XML.",
+  },
+  {
+    active: true,
+    name: "MeNote Android App",
+    image: menoteImg,
+    year: 2015,
+    link: "https://play.google.com/store/apps/details?id=net.noorr.menote.menote",
+    linkText: "Find it on PlayStore",
+    description:
+      "MeNote is a note taking app for Android phones and tablets with functionalities like: write, read, edit, share, delete, undo delete, delete all, set notification reminder, add colors, set different coloring modes, set language (2 languages supported: English, Azerbaijani) etc. MeNote has been written in Java and XML using Android Studio IDE. SQLite has been used as the database. Logo of app has been designed in Adobe Illustrator CC.",
+  },
+  {
+    active: false,
+    name: "Jewellery",
+    image: jewelImg,
+    year: 2014,
+    description:
+      "Some part of design, development of back-end and admin panel of the website - a complete CMS allowing store manager to add, edit, delete items, uploading images, managing other admins etc. Programmed back-end of the website and designed some parts of the front-end to display, sort, search items. HTML, CSS, PS CS6, PHP, MySQL used.",
+  },
+  {
+    active: true,
+    name: "Run Against Gravity",
+    image: runImg,
+    year: 2015,
+    link: "/RunGame",
+    linkText: "Play",
+    description:
+      "A simple 2D endless runner game written in C# language in MonoDevelop IDE and Unity Game Engine. Multi-platform supported (iOS, Android, Windows, Web). After recent updates in other browser, this game is now only playable on Mozilla Firefox browser.",
+  },
+  {
+    active: false,
+    name: "Elandiya.com",
+    image: elandiyaImg,
+    year: 2017,
+    description:
+      "Elandiya.com is a classifieds website intended for Azerbaijan's market. It is built on Wordpress CMS on a predefined theme and help of several plugins, but that is definitely not all: Lots of changes and modifications have been made in php, html, css, js codes, as well as whole new additional codes have been written to bring Elandiya.com to the current view and functionality.",
+  },
+  {
+    active: false,
+    name: "Milkyway Construction Sevices",
+    image: milkywayImg,
+    year: 2016,
+    description:
+      "Milkywaycs.ca - a WordPress website for Milkyway Construction Services Inc. - a company that provides services like tile installation, stone, stucco and masonry stucco in Calgary",
+  },
+  {
+    active: true,
+    name: "Angora Contracting",
+    image: angoraImg,
+    year: 2016,
+    link: "http://angoracontracting.ca/",
+    description:
+      "WordPress powered website for carpeting, flooring company Angora Contracting LTD. in Calgary AB.",
   },
   {
     active: true,
