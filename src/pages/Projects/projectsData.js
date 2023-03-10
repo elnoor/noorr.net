8d4381ac5b8d468be9d53608abada4cafc8b91e9
Elnur Mammadli <elnoor@users.noreply.github.com>
2023-03-09 18:08:07 -0700

    Update projectsData.js

--- src/pages/Projects/projectsData.js
+++ src/pages/Projects/projectsData.js
@@ -17,7 +17,7 @@ export const projectsData = [
     link: "https://metamil.app",
     linkText: "View",
     description:
-      "Meta Milyoner is a quiz competition game like Millionaire TV show. In this game players can also add their own questions which are to be approved by moderators before they can be played by others during the game. Game has 2 modes: Standard mode with 15 serious questions from 3 difficulty levels and Funny mode where questions are meant to be jokes. Tech stack for this game is React Native, JavaScript, .NET 6, C#, SQL Server, Azure. Currently it is only available in Azerbaijani and I have plans to add English soon too. While it can be played online at www.metamil.app, it is also available as native iOS and Android games at AppStore and Play Store.",
+      "Meta Milyoner is a quiz competition game like Millionaire TV show. In this game players can also add their own questions which are to be approved by moderators before they can be played by others during the game. Game has 2 modes: Standard mode with 15 serious questions from 3 difficulty levels and Funny mode where questions are meant to be jokes. Tech stack for this game is React Native, JavaScript, .NET 6, C#, SQL Server, Azure. Currently it is only available in Azerbaijani and I have plans to add English soon too. While it can be played online at metamil.app, it is also available as native iOS and Android games at AppStore and Play Store.",
   },
   {
     active: true,
