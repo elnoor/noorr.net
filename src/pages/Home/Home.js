00e78ae5a85a94fabe8ab3235ea8f31ff40d9bdd
Elnur Mammadli <elnur@elforte.ca>
2025-03-21 15:04:30 -0600

    home page fixed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
-import WelcomeText from "./WelcomeText";
 
 export default function Home() {
-  const [showRevisedText, setShowRevisedText] = useState(true);
   return (
     <div className="text-justified">
       <h1 className="text-center text-light text-opacity-75">Hi there,</h1>
@@ -15,50 +13,33 @@ export default function Home() {
         >
           Webcome&nbsp;
         </a>
-        to my online presence.&nbsp;
-        <WelcomeText showRevisedText={showRevisedText} />
+        to my online presence.&nbsp; My name is Elnur and coding is my true
+        passion. To me, it's not just a way to earn a living, but it brings me
+        joy and fulfillment. When I don't have enough opportunities to code
+        during my work hours due to meetings and events, I like to find new
+        problems to solve and turn them into apps in my free time. More often
+        than not, when I have some spare time, I ask myself "should I play a
+        video game or code something?" and you guessed it, coding wins out 90%
+        of the time. So, don't be surprised if you receive an email from me at
+        3am, I may be busy coding up until then.
+        <br />
+        <br />
+        I enjoy discussing the latest technology trends and sharing my knowledge
+        and insights with others. I believe that knowledge is only valuable when
+        it's shared, and I'm always eager to learn something new. My rule for
+        learning is "meeting twice". When I first encounter something new, I
+        ignore it. But if I see it a second time, I know I must learn it and
+        apply it wherever I can.
+        <br />
+        <br />I may not update this website frequently, but you can check out my
+        LinkedIn profile for a more up-to-date representation of my work and
+        interests. If you have something to share or just want to connect,
+        please don't hesitate to reach out to me through the contact page or on
+        LinkedIn. I'm always open to meeting new people and hearing new ideas.
         <br />
         <br />
         <em>Thank you for stopping by!</em>
       </p>
-
-      <p className="text-end mt-5">
-        I wrote
-        <TextOrLink
-          active={showRevisedText}
-          onClick={() => setShowRevisedText(false)}
-        >
-          this intro
-        </TextOrLink>
-        and
-        <TextOrLink
-          active={!showRevisedText}
-          onClick={() => setShowRevisedText(true)}
-        >
-          ChatGPT revised
-        </TextOrLink>
-        it for me.
-      </p>
     </div>
-  );
-}
-
-function TextOrLink({ children, active, onClick }) {
-  return (
-    <>
-      &nbsp;
-      {active ? (
-        <a
-          onClick={onClick}
-          href="##"
-          className="link-info text-decoration-none"
-        >
-          {children}
-        </a>
-      ) : (
-        children
-      )}
-      &nbsp;
-    </>
   );
 }
